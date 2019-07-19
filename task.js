fbfb5589d37af6cd622aad2fa577ee8ea2c160d0
ZadorozhkoNA <50860897+ZadorozhkoNA@users.noreply.github.com>
2019-07-19 23:57:50 +0300

    Create task.js

---
+++ task.js
@@ -0,0 +1,34 @@
+let form = document.body.querySelector( 'form.tasks__control' );
+let listTasks = document.body.querySelector( '.tasks__list' );
+
+function removeTask( event ) {
+  event.preventDefault();
+  listTasks.removeChild( event.currentTarget.closest( '.task' ) );
+}
+
+function createTask( taskStr, list ) {
+  let divTask = document.createElement( 'div' );
+  divTask.classList.add( 'task' );
+
+  let divTitle = document.createElement( 'div' );
+  divTitle.classList.add( 'task__title' );
+  divTitle.textContent = taskStr;
+
+  let removeTask = document.createElement( 'a' );
+  removeTask.classList.add( 'task__remove' );
+  removeTask.innerHTML = `&times;`;
+  removeTask.addEventListener( 'click', removeTask );
+
+  divTask.appendChild( divTitle );
+  divTask.appendChild( removeTask );
+  list.appendChild( divTask );
+}
+
+document.addEventListener( 'submit', ( event ) => {
+  event.preventDefault();
+  let value = form.querySelector( '#task__input').value;
+  if ( value !== '') {
+    createTask( value, listTasks );
+    form.querySelector( '#task__input').value ='';
+  }
+});
