75b70858fcf088275207bcaffda7e1d229999cd5
ZadorozhkoNA <50860897+ZadorozhkoNA@users.noreply.github.com>
2019-07-20 13:18:44 +0300

    Update task.js
    Изменил название функции removeTask на remove. Возникал конфликт имен.

--- task.js
+++ task.js
@@ -1,7 +1,7 @@
 let form = document.body.querySelector( 'form.tasks__control' );
 let listTasks = document.body.querySelector( '.tasks__list' );
 
-function removeTask( event ) {
+function remove( event ) {
   event.preventDefault();
   listTasks.removeChild( event.currentTarget.closest( '.task' ) );
 }
@@ -17,18 +17,19 @@ function createTask( taskStr, list ) {
   let removeTask = document.createElement( 'a' );
   removeTask.classList.add( 'task__remove' );
   removeTask.innerHTML = `&times;`;
-  removeTask.addEventListener( 'click', removeTask );
+  removeTask.addEventListener( 'click', remove );
 
   divTask.appendChild( divTitle );
   divTask.appendChild( removeTask );
+
   list.appendChild( divTask );
 }
 
 document.addEventListener( 'submit', ( event ) => {
   event.preventDefault();
-  let value = form.querySelector( '#task__input').value;
+  let value = form.querySelector( '#task__input' ).value;
   if ( value !== '') {
     createTask( value, listTasks );
-    form.querySelector( '#task__input').value ='';
+    form.querySelector( '#task__input' ).value ='';
   }
 });
